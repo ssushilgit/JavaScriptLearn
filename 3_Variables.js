a4d5919ad6d6b47c8f15ca35651c4a097571e250
Sushil Shrestha <165645824+ssushilgit@users.noreply.github.com>
2024-04-05 12:30:32 +0545

    first commit

---
+++ 3_Variables.js
@@ -0,0 +1,30 @@
+
+//variables
+console.log("Hello Sushil");
+
+var  fullname = 'Sushil Shrestha'
+console.log(fullname)
+
+price=99.99
+console.log(price)
+
+x=null;
+y=undefined;
+console.log(x);
+console.log(y);
+
+isFollow= false;
+console.log(isFollow)
+
+let book="Harry Potter"
+console.log(book)
+
+let num = 10
+console.log(num)
+
+const mark=66
+console.log(mark)
+
+
+//primitive data types
+
---
+++ 4_Primitives.js
@@ -0,0 +1,26 @@
+//nn bb ss u - Primitives Data types
+let a = null;
+let b = 55;
+let c = true;
+let d = BigInt("567") + BigInt("89")
+let e = "Sushil"
+let f = Symbol("Nice Symbol")
+let g = undefined
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+console.log(g)
+
+
+//objects in js - Non Primitive data types
+const item={
+    "Sushil" : true,
+    "Ritesh" : false,
+    "Saroj" :69,
+    "Dhakal" : undefined
+}
+
+console.log(item["Ritesh"])
