e4bbbc1f07ddb8b8e8bbcaa197b885dc8dc30f4f
Sushil Shrestha <165645824+ssushilgit@users.noreply.github.com>
2024-04-08 00:03:19 +0545

    Practise Set 1

---
+++ 5_PS1.js
@@ -0,0 +1,31 @@
+// Q1
+let a ="Sushil"
+let b = 6
+console.log(a+b)
+
+// Q2
+console.log(typeof(a+b))
+
+// Q3 
+const z={
+    name: "Sushil",
+    section: 1,
+    isPrincipal:false
+}
+
+// z=1
+
+// Q4
+z['friend'] = 'Bhuwan'
+z['name'] = 'Arjun'
+
+console.log(z)
+
+// Q5
+const dict = {
+    Ephemeral: 'lasting for a very short time',
+    Serendipity: 'the occurrence of events by chance in a happy or beneficial way',
+    Perplex: ' to cause someone to feel completely baffled or puzzled.'
+}
+console.log(dict.Ephemeral)
+console.log(dict['Perplex'])
