f1ddb37802e76f33b450682985c38bde5e0c93e5
Sushil Shrestha <165645824+ssushilgit@users.noreply.github.com>
2024-04-28 20:36:46 +0545

    7 - Conditional Expressions

---
+++ 7_Conditional.js
@@ -0,0 +1,36 @@
+// if
+// let a = prompt("What is your age?")
+// a= Number.parseInt(a)
+// if (a>0){
+//     alert("It is a valid age")
+// }
+
+
+// if else
+// let b=prompt("Enter your age")
+// b= Number.parseInt(b);
+// if(b>18){
+//     alert("You are allowed to drive")
+// }
+// else{
+//     alert("You are not elligible to drive")
+// }
+
+//if else .. if
+let marks=prompt("Enter your marks ")
+marks = parseFloat(marks)
+if(marks > 80 && marks <= 100 ){
+    alert("You scored distinction")
+}
+
+else if(marks > 60 && marks <= 80 ){
+    alert("You scored first divison")
+}
+
+else if(marks > 40 && marks <=60){
+    alert("You scored second divison")
+}
+
+else{
+    alert("You are failed")
+}
