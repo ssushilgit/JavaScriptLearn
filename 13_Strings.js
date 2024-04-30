c2d096fdf21fab05254a97623cb6f1bb23a35b50
Sushil Shrestha <165645824+ssushilgit@users.noreply.github.com>
2024-04-30 13:57:56 +0545

    13 - Basic Strings

---
+++ 13_Strings.js
@@ -0,0 +1,26 @@
+//  Basics
+ let names = "Sushil"
+ let namess = 'Shrestha'
+ console.log(names)
+ console.log(namess)
+ console.log(names.length)
+ console.log(names[0])
+ console.log(names[1])
+ console.log(names[2])
+ console.log(names[3])
+ console.log(names[4]) 
+ console.log(names[5])
+
+// Template Literals
+let name1 = "Ritesh"
+let name2 = "Saroj"
+let sentence = `${name1} love ${name2}`
+console.log(sentence) 
+
+
+// Escape Sequence Character
+let p = "Hello ma\'am"
+console.log(p)
+
+let q = "Hello \nma\'am"
+console.log(q)
